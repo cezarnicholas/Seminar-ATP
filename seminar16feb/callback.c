7c88ecf7276acbe0014293093d97571c7aa21887
Cezar Nicholas <cezarnicholas7@gmail.com>
2026-02-16 15:08:05 +0200

    callback

---
+++ seminar16feb/callback.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef int (*Operatie)(int, int);
+
+int aduna(int a, int b) {
+    return a + b;
+}
+
+int scade(int a, int b) {
+    return a - b;
+}
+
+int inmulteste(int a, int b) {
+    return a * b;
+}
+
+int imparte(int a, int b) {
+    return a / b;
+}
+
+// functie generica care accepta un callback
+int aplica_operatie(int x, int y, Operatie op) {
+    return op(x, y);
+}
+
+int main() {
+    int a = 40, b = 20;
+    // 40 + 20 = 60
+    printf("%d + %d = %d\n", a, b, aplica_operatie(a, b, aduna));
+}
