578e6607d37f412968183ffee08423dffa5f400c
cezarnicholas <cezarnicholas7@gmail.com>
2026-03-23 12:59:39 +0200

    23mar

---
+++ seminar-23mar/1.c
@@ -0,0 +1,31 @@
+// Sa se scrie subprogramul recursiv care determina numarul de elemente negative dintr-un vector.
+// Se va scrie si programul apelator.
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_negatives(int *v, int n) {
+    int rez;
+    if (n == 0) {
+        rez = 0;
+    }
+    else {
+        rez = count_negatives(v, n - 1);
+        if (v[n - 1] < 0)
+            rez += 1;
+    }
+
+    return rez;
+}
+
+int main(int argc, char *argv[]) {
+    int n;
+    scanf("%d", &n);
+
+    int *v = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        scanf("%d", &v[i]);
+
+    int ans = count_negatives(v, n);
+
+    printf("%d\n", ans);
+}
